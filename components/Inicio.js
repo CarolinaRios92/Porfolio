ad8ec13429824684ce595f5eefbc2617df6dc956
unknown <caritu_rios@hotmail.com>
2023-08-08 13:50:54 -0300

    inicio

--- components/Inicio.js
+++ components/Inicio.js
@@ -1,26 +1,40 @@
 import Image from "next/image"
-import Link from "next/link"
+import { TypeAnimation } from "react-type-animation"
 
 export const Inicio = () => {
     return (
-        <section className="flex justify-center items-center gap-5">
-            <div>
-                <h1>Leandra Carolina Ríos</h1>
-                <h2>Hola! Soy Full Stack Web Developer</h2>
+        <section className="flex justify-center items-center content-start gap-16">
+            <div className="flex flex-col gap-3">
+                <h1 className="text-5xl font-bold">Leandra Carolina Ríos</h1>
+                <h2 className="text-lg">Hola! Soy 
+                    <TypeAnimation
+                        sequence={[
+                            "_Full Stack Web Developer",
+                            1000,
+                            "_Front-end Developer",
+                            1000,
+                            "_Back-end Developer",
+                            1000,
+                        ]}
+                        speed={50}
+                        wrapper="SectionText"
+                        repeat={Infinity}
+                    />
+                </h2>
             </div>
             
             <div>
-                <div>
-                    <a href="#contact">Contactame!</a>
-                    <a>Aquí te dejo mi CV</a>
+                <div className="flex flex-col py-7 gap-3">
+                    <a href="#contact" className="bg-primary text-center py-1 rounded-lg hover:bg-transparent hover:border-primary hover:border-2">Contactame!</a>
+                    <a href="/CV_Leandra_Carolina_Ríos_Full_Stack_Web_Developer.pdf" target='_blank' className="bg-secondary text-center py-1 rounded-lg text-white px-3 hover:bg-transparent hover:border-secondary hover:border-2 hover:text-black">Aquí te dejo mi CV</a>
                 </div>
                 
-                <div className="flex gap-5">
-                    <a href="https://www.linkedin.com/in/leandra-carolina-rios-431965151/">
-                        <Image src="/images/Logos/linkedin.png" alt="linkedin" width={40} height={40}/>
+                <div className="flex gap-5 items-center justify-center">
+                    <a href="https://www.linkedin.com/in/leandra-carolina-rios-431965151/" target='_blank'>
+                        <Image src="/images/Logos/linkedin.png" alt="linkedin" width={30} height={30}/>
                     </a>
-                    <a href="https://github.com/CarolinaRios92">
-                         <Image src="/images/Logos/github.png" alt="github" width={40} height={40}/>
+                    <a href="https://github.com/CarolinaRios92" target='_blank'>
+                         <Image src="/images/Logos/github.png" alt="github" width={30} height={30}/>
                     </a>
                 </div>
 
--- components/Projects.js
+++ components/Projects.js
@@ -140,7 +140,7 @@ export const Projects = () => {
         }
     ]
     return (
-        <section id="projects" >
+        <section id="projects">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#E5989B" fill-opacity="1" d="M0,224L480,96L960,224L1440,160L1440,320L960,320L480,320L0,320Z"></path>
             </svg>
