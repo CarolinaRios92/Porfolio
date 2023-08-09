711524372445c9c3fcc05d37cd76e34ac06b4926
unknown <caritu_rios@hotmail.com>
2023-08-09 15:25:58 -0300

    contact

--- components/Contact.js
+++ components/Contact.js
@@ -1,5 +1,63 @@
+import { useState } from "react";
+
 export const Contact = () => {
+    const [name, setName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+
     return (
-        <form id="contact">Contact</form>
+        <div id="contact" className="w-3/5 m-auto mt-10 bg-secondary p-7">
+            <h3 className="text-3xl font-medium pb-2">_Contáctame</h3>
+            <form className="flex flex-col">
+                <div className="flex gap-3 pt-3">
+                    <div className="basis-1/2 flex">
+                        <label className="basis-1/5">Nombre: </label>
+                        <input 
+                            type="text"
+                            placeholder="Nombre..."
+                            className="p-1 rounded-md basis-4/5"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}/>
+                    </div>
+                    
+                    <div className="basis-1/2">
+                        <label className="pr-2">Apellido: </label>
+                        <input 
+                            type="text"
+                            placeholder="Apellido..."
+                            className="p-1 rounded-md"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}/>
+                    </div>
+                </div>
+                
+                <div className="pt-3">
+                    <label className="pr-2">Asunto: </label>
+                    <input 
+                        type="text"
+                        placeholder="Asunto..."
+                        className="p-1 rounded-md"
+                        value={subject}
+                        onChange={(e) => setSubject(e.target.value)}/>
+                </div>
+                
+                <div className="pt-3">
+                    <label className="pr-2">Mensaje: </label>
+                    <input 
+                        type="text"
+                        placeholder="Mensaje..."
+                        className="p-1 rounded-md"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}/>
+                </div>
+                
+
+                <button
+                    type="submit">
+                    Enviar!
+                </button>
+            </form>
+        </div>
     )
 }
--- components/Projects.js
+++ components/Projects.js
@@ -14,7 +14,7 @@ export const Projects = () => {
     }
     console.log(project)
     return (
-        <section id="projects" className="pt-0 mt-0">
+        <section className="pt-0 mt-0">
             
             <svg className="mb-0 pt-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#9381ff" fill-opacity="1" d="M0,224L48,229.3C96,235,192,245,288,245.3C384,245,480,235,576,208C672,181,768,139,864,122.7C960,107,1056,117,1152,144C1248,171,1344,213,1392,234.7L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
@@ -23,7 +23,7 @@ export const Projects = () => {
 
             <div className="bg-primary mt-0 pt-0 border-none"> 
                 <div className="w-3/5 m-auto pt-10">
-                    <h3 className="text-3xl font-medium pb-3 text-white">
+                    <h3 id="projects" className="text-3xl font-medium pb-3 text-white">
                         _Proyectos
                     </h3>
 
