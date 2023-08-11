66419e44cf3399e321bcd815f47699d01b279d0b
unknown <caritu_rios@hotmail.com>
2023-08-11 15:09:36 -0300

    scroll

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" className='sr'>
+    <Html lang="en" className='sr scroll-smooth'>
       <Head>
         <title>Carolina Ríos</title>
         <link rel="shortcut icon" href='/favicon/favicon.ico' />
